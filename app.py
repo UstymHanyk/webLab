7923fd20bfffa9f2653638fc9378db6f18366660
Lajob <ustym.hanyk@ukr.net>
2023-10-12 23:47:57 +0300

    laba five

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+gemstones = [
+    {"id": 1, "name": "Diamond", "carats": 2, "price": 3342},
+    {"id": 2, "name": "Emerald", "carats": 5, "price": 124},
+    {"id": 3, "name": "Ruby", "carats": 32, "price": 222222}
+]
+next_id = 4  
+
+# CRUD operations
+@app.route('/gemstones', methods=['GET'])
+def get_gemstones():
+    return jsonify({'gemstones': gemstones})
+
+@app.route('/gemstones/<int:gemstone_id>', methods=['GET'])
+def get_gemstone(gemstone_id):
+    gemstone = next((gem for gem in gemstones if gem['id'] == gemstone_id), None)
+    if gemstone:
+        return jsonify({'gemstone': gemstone})
+    return jsonify({'message': 'Gemstone not found'}), 404
+
+@app.route('/gemstones', methods=['POST'])
+def add_gemstone():
+    global next_id
+    new_gemstone = {
+        'id': next_id,
+        'name': request.json['name'],
+        'carats': request.json['carats'],
+        'price': request.json['price']
+    }
+    gemstones.append(new_gemstone)
+    next_id += 1
+    return jsonify({'message': 'Gemstone added', 'gemstone': new_gemstone}), 201
+
+@app.route('/gemstones/<int:gemstone_id>', methods=['PUT'])
+def update_gemstone(gemstone_id):
+    gemstone = next((gem for gem in gemstones if gem['id'] == gemstone_id), None)
+    if gemstone:
+        gemstone['name'] = request.json['name']
+        gemstone['carats'] = request.json['carats']
+        gemstone['price'] = request.json['price']
+        return jsonify({'message': 'Gemstone updated', 'gemstone': gemstone})
+    return jsonify({'message': 'Gemstone not found'}), 404
+
+@app.route('/gemstones/<int:gemstone_id>', methods=['DELETE'])
+def delete_gemstone(gemstone_id):
+    global gemstones
+    gemstones = [gem for gem in gemstones if gem['id'] != gemstone_id]
+    return jsonify({'message': 'Gemstone deleted'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- script.js
+++ script.js
@@ -1,64 +1,72 @@
-class Gemstone {
-  constructor(name, carats, price) {
-    this.name = name;
-    this.carats = carats;
-    this.price = price;
-  }
-}
-
 const main = document.getElementById('main');
 const sortBtn = document.getElementById('sort');
 const searchInput = document.getElementById('search');
+let gemstonesData = [];  // Store all gemstones
 
+function updateDOM() {
+  const searchTerm = searchInput.value.toLowerCase();
+  const filteredGemstones = gemstonesData.filter(item => item.name.toLowerCase().includes(searchTerm));
 
-let data = [
-  new Gemstone('Diamond', 2, 3342),
-  new Gemstone('Emerald', 5, 124),
-  new Gemstone('Ruby', 32, 222222)
-];
-
-function updateDOM(providedData = data) {
   main.innerHTML = '<h2>Gemstone List</h2>';
 
-  providedData.forEach((item, index) => {
+  filteredGemstones.forEach(item => {
     const element = document.createElement('div');
     element.classList.add('gemstone');
     element.innerHTML = `
       <strong>${item.name}</strong> - Carats: ${item.carats}, Price: ${item.price}
-      <button class="edit-button" onclick="redirectToEditPage(${index})">Edit</button>
+      <button class="edit-button" onclick="redirectToEditPage(${item.id})">Edit</button>
+      <button class="remove-button" onclick="removeGemstone(${item.id})">Remove</button>
     `;
     main.appendChild(element);
   });
 }
 
-function redirectToEditPage(index) {
-  window.location.href = `edit_gemstone.html?index=${index}`;
+function redirectToEditPage(gemstoneId) {
+  window.location.href = `edit_gemstone.html?id=${gemstoneId}`;
 }
 
-
-
-function sortByPrice() {
-  data.sort((a, b) => a.price - b.price);
-  updateDOM();
-}
-
-function searchGemstone() {
-  const searchTerm = searchInput.value.toLowerCase();
-  const filteredGemstones = data.filter(gemstone =>
-    gemstone.name.toLowerCase().includes(searchTerm)
-  );
-  updateDOM(filteredGemstones);
+function redirectToAddPage() {
+  window.location.href = `add_gemstone.html`;
 }
 
 function loadGemstones() {
-  const gemstones = JSON.parse(localStorage.getItem('gemstones')) || [];
-  data = gemstones.map(gemstone => new Gemstone(gemstone.name, gemstone.carats, gemstone.price));
-  updateDOM();
+  fetch('http://localhost:5000/gemstones')
+    .then(response => response.json())
+    .then(data => {
+      gemstonesData = data.gemstones;  // Store gemstones data
+      updateDOM();  // Initial rendering
+    })
+    .catch(error => console.error('Error fetching gemstones:', error));
 }
 
+function removeGemstone(gemstoneId) {
+  fetch(`http://localhost:5000/gemstones/${gemstoneId}`, {
+    method: 'DELETE'
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Remove the gemstone from gemstonesData
+      gemstonesData = gemstonesData.filter(item => item.id !== gemstoneId);
+
+      // Update the DOM to reflect the changes
+      updateDOM();
+    })
+    .catch(error => console.error('Error deleting gemstone:', error));
+}
+
+
 function init() {
-  sortBtn.addEventListener('click', sortByPrice);
-  searchInput.addEventListener('input', searchGemstone);
+  sortBtn.addEventListener('click', () => {
+    const sortedGemstones = gemstonesData.slice().sort((a, b) => a.price - b.price);
+    updateDOM(sortedGemstones);
+  });
+
+  searchInput.addEventListener('input', updateDOM);
 
   loadGemstones();
 }
