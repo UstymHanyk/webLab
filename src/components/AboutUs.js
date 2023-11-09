1a6537fec9b8da57c2e44ebbc81673835e271d6c
UstymHanyk <ustym.hanyk@ukr.net>
2023-11-10 00:33:21 +0200

    added new components

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,43 +1,50 @@
 import React from 'react';
 import '../styles/AboutUs.css';
+import AboutUsCard from './AboutUsCard'; // Import the AboutUsCard component
 
 const AboutUs = () => {
+  const cardData = [
+    {
+      title: 'Gemstones Sold:',
+      text: 'Over 100,000 gemstones have found new homes with our delighted customers.',
+    },
+    {
+      title: 'On The Market:',
+      text: "We've been proudly serving gemstone enthusiasts since 1987.",
+    },
+    {
+      title: 'Customer Rating:',
+      text: 'Our commitment to quality has earned us a 94% satisfaction rate among our customers.',
+    },
+    {
+      title: 'Gemstone Variety:',
+      text: 'We offer over 500 unique types of gemstones, ensuring a wide selection for our customers.',
+    },
+    {
+      title: 'Expert Gemologists:',
+      text: 'Our team of certified gemstone experts ensures the highest quality and authenticity of our gemstones.',
+    },
+    {
+      title: 'Worldwide Shipping:',
+      text: 'We provide worldwide shipping to over 100 countries, so you can enjoy our gemstones anywhere in the world.',
+    },
+    {
+      title: 'Quality Assurance:',
+      text: "Our commitment to 100% genuine gemstones ensures that you're getting the best quality products.",
+    },
+    {
+      title: 'Customer Support:',
+      text: 'Our dedicated customer support team is available 24/7 to assist you with any inquiries or assistance you may need.',
+    },
+  ];
+
   return (
     <div className="about-us">
       <h2><span className="underline-light_purple">About us</span></h2>
       <div className="about-us__container_wrapper">
-        <div className="about-us__card">
-          <p className="about-us__card__title">Gemstones Sold:</p>
-          <p className="about-us__card__text">Over 100,000 gemstones have found new homes with our delighted customers.</p>
-        </div>
-        <div className="about-us__card">
-          <p className="about-us__card__title">On The Market:</p>
-          <p className="about-us__card__text">We've been proudly serving gemstone enthusiasts since 1987.</p>
-        </div>
-        <div className="about-us__card">
-          <p className="about-us__card__title">Customer Rating:</p>
-          <p className="about-us__card__text">Our commitment to quality has earned us a 94% satisfaction rate among our customers.</p>
-        </div>
-        <div className="about-us__card">
-          <p className="about-us__card__title">Gemstone Variety:</p>
-          <p className="about-us__card__text">We offer over 500 unique types of gemstones, ensuring a wide selection for our customers.</p>
-        </div>
-        <div className="about-us__card">
-          <p className="about-us__card__title">Expert Gemologists:</p>
-          <p className="about-us__card__text">Our team of certified gemstone experts ensures the highest quality and authenticity of our gemstones.</p>
-        </div>
-        <div className="about-us__card">
-          <p className="about-us__card__title">Worldwide Shipping:</p>
-          <p className="about-us__card__text">We provide worldwide shipping to over 100 countries, so you can enjoy our gemstones anywhere in the world.</p>
-        </div>
-        <div className="about-us__card">
-          <p className="about-us__card__title">Quality Assurance:</p>
-          <p className="about-us__card__text">Our commitment to 100% genuine gemstones ensures that you're getting the best quality products.</p>
-        </div>
-        <div className="about-us__card">
-          <p className="about-us__card__title">Customer Support:</p>
-          <p className="about-us__card__text">Our dedicated customer support team is available 24/7 to assist you with any inquiries or assistance you may need.</p>
-        </div>
+        {cardData.map((card, index) => (
+          <AboutUsCard key={index} title={card.title} text={card.text} />
+        ))}
       </div>
     </div>
   );
--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,50 +1,39 @@
 import React from 'react';
 import '../styles/FAQ.css';
+import FAQItem from './FAQItem'; 
 
 const FAQ = () => {
+  const faqData = [
+    {
+      question: 'What are the different types of gemstones you offer?',
+      answer: 'We offer a wide variety of gemstones, including but not limited to diamonds, rubies, sapphires, emeralds, and more. You can explore our complete gemstone catalog for more details.',
+    },
+    {
+      question: 'Do you offer custom jewelry designs with gemstones?',
+      answer: 'Yes, we provide custom jewelry design services. You can work with our designers to create unique jewelry pieces with your chosen gemstones.',
+    },
+    {
+      question: 'What is the return policy for gemstone purchases?',
+      answer: 'Our return policy allows you to return gemstone purchases within 30 days of receipt. Please review our detailed return policy for more information.',
+    },
+    {
+      question: 'Are your gemstones certified for authenticity?',
+      answer: 'Yes, all our gemstones are certified for authenticity. We provide gemstone certificates to ensure the quality and authenticity of our products.',
+    },
+  ];
+
   return (
     <div id="section-faq" className="faq-section">
       <div className="faq-contents">
         <div className="faq-left">
-        <h2>Frequently Asked <span class="underline-purple">Questions</span></h2>
+          <h2>Frequently Asked <span className="underline-purple">Questions</span></h2>
         </div>
         <div className="faq-right">
           <div className="w-dyn-list">
             <div role="list" className="w-dyn-items">
-              <div role="listitem" className="faq-wrapper w-dyn-item">
-                <div className="faq-dropdown w-dropdown">
-                  <div className="faq-toggle w-dropdown-toggle">
-                    <div>What are the different types of gemstones you offer?</div>
-                  </div>
-                  <p>We offer a wide variety of gemstones, including but not limited to diamonds, rubies, sapphires, emeralds, and more. You can explore our complete gemstone catalog for more details.</p>
-                </div>
-              </div>
-              <div role="listitem" className="faq-wrapper w-dyn-item">
-                <div className="faq-dropdown w-dropdown">
-                  <div className="faq-toggle w-dropdown-toggle">
-                    <div>Do you offer custom jewelry designs with gemstones?</div>
-                  </div>
-                  <p>Yes, we provide custom jewelry design services. You can work with our designers to create unique jewelry pieces with your chosen gemstones.</p>
-                </div>
-              </div>
-
-              <div role="listitem" className="faq-wrapper w-dyn-item">
-                <div className="faq-dropdown w-dropdown">
-                  <div className="faq-toggle w-dropdown-toggle">
-                    <div>What is the return policy for gemstone purchases?</div>
-                  </div>
-                  <p>Our return policy allows you to return gemstone purchases within 30 days of receipt. Please review our detailed return policy for more information.</p>
-                </div>
-              </div>
-
-              <div role="listitem" className="faq-wrapper w-dyn-item">
-                <div className="faq-dropdown w-dropdown">
-                  <div className="faq-toggle w-dropdown-toggle">
-                    <div>Are your gemstones certified for authenticity?</div>
-                  </div>
-                  <p>Yes, all our gemstones are certified for authenticity. We provide gemstone certificates to ensure the quality and authenticity of our products.</p>
-                </div>
-              </div>
+              {faqData.map((faq, index) => (
+                <FAQItem key={index} question={faq.question} answer={faq.answer} />
+              ))}
             </div>
           </div>
         </div>
